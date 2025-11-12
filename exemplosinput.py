9ac064e64875514069b710091bf6ee43ed323121
Cristayfer <cristayferaraujo@gmail.com>
2025-11-11 23:37:08 -0300

    commit aulas

---
+++ exemplosinput.py
@@ -0,0 +1,8 @@
+# Lê o valor digitado pelo usuário (vem como texto)
+numero = input("Digite um número: ")
+
+# Converte o texto para número inteiro
+numero = int(numero)
+
+# Agora podemos usar o número em cálculos
+print("O dobro do número é:", numero * 2)
