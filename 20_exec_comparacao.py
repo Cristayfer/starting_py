e713db5e3b11d42c813c01f2f9cb717ffd2191b7
Cristayfer <cristayferaraujo@gmail.com>
2025-11-12 22:07:48 -0300

    commit aulas

--- 20_exec_comparacao.py
+++ 20_exec_comparacao.py
@@ -3,5 +3,9 @@ segundo_valor = input('Digite outro valor: ')
 
 if primeiro_valor > segundo_valor:
     print(f'{primeiro_valor=} é maior do que {segundo_valor}')
+
+elif primeiro_valor == segundo_valor:
+    print(f'o {primeiro_valor} é igual ao {segundo_valor}')
+
 else:
     print(f'{segundo_valor} é maior do que {primeiro_valor}')
