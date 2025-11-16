2d507a396affed065a13ac783aad9b2ccf0c3a04
Cristayfer <cristayferaraujo@gmail.com>
2025-11-16 17:56:31 -0300

    commit aula

---
+++ 25_str_interpolacao.py
@@ -0,0 +1,13 @@
+'''
+Interpretação básica de strings
+s - string
+d e i - int
+f - float
+x e X - Hexadecimal (ABCDEF0123456789)
+'''
+
+nome = 'Cris'
+preco = 1000.9589
+variavel = '%s, o preço é R$%f' % (nome, preco)
+print(variavel)
+print('O hexadecimal de %d é %08X' % (1500, 1500))
